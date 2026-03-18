b597cde8bce1acaac63c9a991221e964c976e9d1
saracollu <saracollu.ws@gmail.com>
2026-03-18 18:17:41 +0100

    filtraIniziali

--- 02_iniziali/script.js
+++ 02_iniziali/script.js
@@ -6,8 +6,25 @@ const names = ["Anna", "Luca", "Marco", "Adele", "Giovanni", "Alessandra"];
 // Dichiara la funzione qui.
 
 
+
+function getInitials(names) {
+
+const initials = [];
+
+    for ( i = 0 ; i < names.length ; i++ ) {
+        
+        const everyName = names[i]
+        const everyInitials = everyName[0]
+        initials.push(everyInitials)
+       
+    }
+  return initials
+}
+
 // Invoca la funzione qui e stampa il risultato in console
 
+const allInitials = getInitials(names);
+console.log(allInitials);
 
 
 //Risultato atteso: ["A", "L", "M", "A", "G", "A"]
--- 03_filtra_iniziali/script.js
+++ 03_filtra_iniziali/script.js
@@ -3,11 +3,25 @@
 const names = ["Anna", "Luca", "Marco", "Adele", "Laura", "Alessandra"];
 
 
+
 // Dichiara la funzione qui.
 
+function getNames (array , letter) {
 
-// Invoca la funzione qui e stampa il risultato in console
+   const initialsL = [];
 
+    for (i = 0 ; i < array.length ; i++) {
+        if (array[i][0] === letter) {
+            initialsL.push(array[i])
+        }
+    }
+    return initialsL
+}
+
+
+const initialsL = getNames(names , "L")
+
+console.log(initialsL);
 
 
 //Risultato atteso se si passa la lettera A: ["Anna", "Adele", "Alessandra"]
