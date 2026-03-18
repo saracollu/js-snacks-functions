c6803a4b0436e92d243199c4ac775c8a55daf14a
saracollu <saracollu.ws@gmail.com>
2026-03-18 20:51:44 +0100

    bonus saluti

--- 04_conta_vocali/script.js
+++ 04_conta_vocali/script.js
@@ -5,9 +5,39 @@ const word = 'javascript';
 
 // Dichiara la funzione qui.
 
+function onlyVocals (array){
+
+    
+    
+    const letters = array.split(''); 
+    const vocals = ['a' , 'e' , 'i' , 'o' , 'u']; //per confrontare
+    console.log(vocals);
+    
+    console.log(letters);
+
+    const howManyVocals = [] //dove inserire le vocali di word
+
+    for (let i = 0 ; i < array.length ; i++) {
+        
+        console.log('ciao');
+       
+        
+        if (vocals.includes(letters[i])){
+
+           howManyVocals.push(letters[i])
+           
+           
+        }
+    
+    }
+
+    return howManyVocals
+}
 
 // Invoca la funzione qui e stampa il risultato in console
 
+const howManyVocals = onlyVocals(word)
+console.log(howManyVocals.length);
 
 
 //Risultato atteso se si passa 'javascript': 3 (a, a, i)
--- 05_bonus/script.js
+++ 05_bonus/script.js
@@ -5,13 +5,34 @@ buon pomeriggio se è pomeriggio (fino alle 17)
 e buonasera se è sera (oltre le 17)
 */
 
-const name = 'Mario';
+const nameM = 'Mario';
+let hello = ['Buongiorno','Buon pomeriggio','Buonasera']
 
+let orario = prompt("Che ora è?" , "Mattina" , "Pomeriggio" , "Sera" )
 
 // Dichiara la funzione qui.
+function salutVar (nome) {
+    for (let i = 0 ; i < hello.length ; i++) {
+    if (orario === "Mattina") {
+        console.log(hello[0] + " " + nome);
+        
+    } else if (orario === "Pomeriggio") {
+        console.log(hello[1] + " " + nome);
+        
+    } else if (orario = "Sera") {
+        console.log(hello[2] + " " + nome);
+        
+    }
+    return hello
+}
+}
+    
 
+    
 
 // Invoca la funzione qui e stampa il risultato in console
+
+let result = salutVar(nameM)
 
 
 
